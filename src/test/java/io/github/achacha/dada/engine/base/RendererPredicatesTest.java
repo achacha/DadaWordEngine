ad593e02c0e47d0722e3341a5f749abb7d621e60
Alex Chachanashvili <achacha@hotmail.com>
2020-11-28 16:43:59 -0600

    Predicate tests;

--- src/test/java/io/github/achacha/dada/engine/base/RendererPredicatesTest.java
+++ src/test/java/io/github/achacha/dada/engine/base/RendererPredicatesTest.java
@@ -5,6 +5,7 @@ import io.github.achacha.dada.test.GlobalTestData;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class RendererPredicatesTest {
 
@@ -12,11 +13,43 @@ public class RendererPredicatesTest {
     public void truePercent() {
         SentenceRendererBuilder sr = new SentenceRendererBuilder(GlobalTestData.WORD_DATA)
                 .nounBuilder()
-                    .withFallback("-should-always-happen-", RendererPredicates.truePercent(100))
+                    .withFallback("-should-always-happen-", RendererPredicates.trueIfPercent(100))
                     .build();
 
         for (int i=0; i<100; ++i) {
             assertEquals("-should-always-happen-", sr.execute());   // 100 tries
         }
+    }
+
+    @Test
+    public void trueProbability() {
+        SentenceRendererBuilder sr = new SentenceRendererBuilder(GlobalTestData.WORD_DATA)
+                .nounBuilder()
+                .withFallback("-should-never-happen-", RendererPredicates.trueIfProbability(0.0))
+                .build();
+
+        for (int i=0; i<100; ++i) {
+            assertNotEquals("-should-never-happen-", sr.execute());   // 100 tries
+        }
+    }
+
+    @Test
+    public void trueAlways() {
+        SentenceRendererBuilder sr = new SentenceRendererBuilder(GlobalTestData.WORD_DATA)
+                .nounBuilder()
+                .withFallback("-should-always-happen-", RendererPredicates.trueAlways())
+                .build();
+
+        assertEquals("-should-always-happen-", sr.execute());
+    }
+
+    @Test
+    public void trueNever() {
+        SentenceRendererBuilder sr = new SentenceRendererBuilder(GlobalTestData.WORD_DATA)
+                .nounBuilder()
+                .withFallback("-should-never-happen-", RendererPredicates.falseAlways())
+                .build();
+
+        assertNotEquals("-should-never-happen-", sr.execute());
     }
 }
